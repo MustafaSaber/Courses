9073ae62e9c7cbae48a4b0a7783217a2bb8b6333
Mostafa Saber <mostafa.saber3789@gmail.com>
2018-09-07 18:13:58 +0200

    refactor

--- ex3 - NN and Multiclass classification/lrCostFunction.m
+++ ex3 - NN and Multiclass classification/lrCostFunction.m
@@ -41,7 +41,6 @@ h = sigmoid(X * theta);
 J = -1/m * ((y' * log(h)) + ((1-y)' * log(1-h))) + (lambda/(2*m))* (NewTheta' * NewTheta);
 grad = (1/m) * ((X' * (h - y)) + (lambda)*[0; NewTheta]);
 
-
-
+grad = grad(:);
 
 end
--- ex3 - NN and Multiclass classification/oneVsAll.m
+++ ex3 - NN and Multiclass classification/oneVsAll.m
@@ -55,7 +55,7 @@ options = optimset('GradObj' , 'on' , 'MaxIter' , 50);
 for c = 1:num_labels,
   [theta] = fmincg (@(t)(lrCostFunction(t, X , (y==c) , lambda)) , initial_theta , options);
   all_theta(c,:) = theta';
-end
+endfor
 
 
 end
